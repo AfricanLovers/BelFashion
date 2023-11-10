95968e0d8a2cf68dc6fe11ca3adf332c8ca6b529
Кочанова Наталья Ивановна <148565980+EgorUML@users.noreply.github.com>
2023-11-10 11:14:23 +0300

    Основной интерфейс

---
+++ model/order_details_view.py
@@ -0,0 +1,49 @@
+from PyQt5.QtWidgets import QMainWindow
+from loader import db
+from model.order_details_clothes_card import OrderDetailsClothesCard
+from design.layouts.order_details_layout import Ui_OrderDetails
+
+
+class OrderDetailsView(QMainWindow, Ui_OrderDetails):
+
+    def __init__(self, *args, main_window=None):
+        super(OrderDetailsView, self).__init__(*args)
+        self.setupUi(self)
+
+        self.main_window = main_window
+        self.order_id = -1
+        self.parent_window = None
+
+        self.backBtn.clicked.connect(self.go_back)
+
+    def set_order_id(self, order_id):
+        self.order_id = order_id
+        self.load_order_details()
+
+    def set_parent_window(self, parent_window):
+        self.parent_window = parent_window
+
+    def load_order_details(self):
+
+        if self.order_id == -1: return
+
+        for item in db.get_order_items_by_order_id(self.order_id):
+            clothes = db.get_clothes_by_id(item[2])
+            if clothes is None: return
+
+            quantity = item[3]
+            clothes_card = OrderDetailsClothesCard(clothes[0], clothes[2], clothes[3], db.get_category_name(clothes[1]), clothes[4], clothes[7], clothes[6], quantity, self.main_window, parent_window=self)
+            self.clothesLayout.addWidget(clothes_card)
+
+    def clear_products(self):
+        layout = self.clothesLayout.layout()
+
+        for i in reversed(range(layout.count())):
+            widget = layout.itemAt(i).widget()
+            if widget is not None:
+                widget.deleteLater()
+
+    def go_back(self):
+        if self.main_window is not None:
+            self.main_window.stacked_widget.setCurrentWidget(self.parent_window)
+            self.clear_products()
---
+++ model/order_placement.py
@@ -0,0 +1,76 @@
+from PyQt5.QtWidgets import QMainWindow
+from design.layouts.order_placement_layout import Ui_OrderPlacement
+from loader import db, cart
+from .helper import show_error_message, show_success_message
+import re
+
+
+EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+PHONE_REGEX = re.compile(r"^\+?\d{1,4}[\s\-]?\(?\d{1,3}?\)?[\s\-]?\d{1,4}[\s\-]?\d{1,4}[\s\-]?\d{1,4}$")
+
+
+class OrderPlacement(QMainWindow, Ui_OrderPlacement):
+
+    def __init__(self, *args, main_window=None, current_user_id=-1):
+        super(OrderPlacement, self).__init__(*args)
+        self.setupUi(self)
+
+        self.main_window = main_window
+        self.current_user_id = current_user_id
+        self.price = 0
+
+        self.confirmOrderButton.clicked.connect(self.confirm_order)
+        self.cancelButton.clicked.connect(self.cancel_order)
+
+    def set_total_price(self, price):
+        self.price = price
+
+    def set_user_id(self, user_id):
+        self.current_user_id = user_id
+
+    def get_address(self):
+        return self.addressLineEdit.text().strip()
+
+    def get_email(self):
+        email = self.emailLineEdit.text().strip()
+        return email if EMAIL_REGEX.match(email) else None
+
+    def get_phone(self):
+        phone = self.phoneLineEdit.text().strip()
+        return phone if PHONE_REGEX.match(phone) else None
+
+    def confirm_order(self):
+        address = self.get_address()
+        email = self.get_email()
+        phone = self.get_phone()
+        is_checked = self.termsCheckBox.isChecked()
+
+        if not address:
+            show_error_message("Пожалуйста, введите правильное имя.")
+            return
+
+        if not email:
+            show_error_message("Пожалуйста, введите действительный адрес электронной почты.")
+            return
+
+        if not phone:
+            show_error_message("Пожалуйста, введите действительный номер телефона.")
+            return
+
+        if not is_checked:
+            show_error_message("Пожалуйста, согласитесь с условиями.")
+            return
+
+        order_id = db.add_order(self.current_user_id, self.price, address, email, phone, self.paymentComboBox.currentIndex())
+        for clothes in cart.get_clothes():
+            db.add_order_item(order_id, clothes[0], clothes[1])
+
+        show_success_message("Ваш заказ успешно принят!")
+        cart.clear_cart()
+        self.main_window.mainMenu.clear_products()
+        self.main_window.mainMenu.load_clothes()
+        self.main_window.stacked_widget.setCurrentWidget(self.main_window.mainMenu)
+
+    def cancel_order(self):
+        if self.main_window is not None:
+            self.main_window.stacked_widget.setCurrentWidget(self.main_window.cart)
